420567653c9bb6e65e4a0c333bd5a7cbe15610f2
Eunsung Choi <ens454@naver.com>
2023-11-21 21:42:28 +0900

    first commit

---
+++ GgoGgu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GgoGgu
+//
+//  Created by 최은성 on 2023/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

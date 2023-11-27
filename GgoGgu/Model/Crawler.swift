35679b1c98b2ee06c09ca6754500507a939a1dbb
Eunsung Choi <ens454@naver.com>
2023-11-28 00:47:15 +0900

    Refactor: 프로젝트 구조 변경

---
+++ GgoGgu/Model/Crawler.swift
@@ -0,0 +1,17 @@
+//
+//  Crawler.swift
+//  GgoGgu
+//
+//  Created by 최은성 on 2023/11/28.
+//
+
+import Foundation
+import SwiftSoup
+
+class Crawler {
+    func crawl() -> [String] {
+        var numbers: [String] = []
+
+        return numbers
+    }
+}
